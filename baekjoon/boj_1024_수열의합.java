ee9338c9b1803611f792ab9d1139d8af401e844c
parkmm14 <palswo@naver.com>
2021-01-25 17:05:00 +0900

    algorithm

--- baekjoon/boj_1024_수열의합.java
+++ baekjoon/boj_1024_수열의합.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 public class boj_1024_수열의합 {
-	
+
 		public static void main(String []args) throws Exception {
 	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	        
@@ -20,7 +20,7 @@ public class boj_1024_수열의합 {
 	            System.out.println(start);
 	            if(start < 0) break;
 	            
-	            
+			//a1~an항의 합 구하기. 주의! / 연산에서 소수점이하 무시되니까 (L/2) 이렇게 괄호쓰면 잘못된 답 얻어짐.
 	            if((2*start + L - 1)*L/2 == N) {
 	                for(long idx = 0; idx < L; idx++) {
 	                    long num = start + idx;
