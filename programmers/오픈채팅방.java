a8155679805f1b2601eeb1e356ceda7dbd5886b0
parkmm14 <palswo@naver.com>
2021-02-13 18:24:08 +0900

    programmers

---
+++ programmers/오픈채팅방.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class openChatting {
+    public String[] solution(String[] record) {
+        HashMap<String, String> info = new HashMap<>();
+        ArrayList<String> chatLog = new ArrayList<>();
+        String[] answer = {};
+
+        for(String str : record) {
+            String[] input = str.split(" ");
+            String cmd = input[0];
+            String uid = input[1];
+            String nickName = "";
+            if(!cmd.equals("Leave"))
+                nickName = input[2];
+            switch(cmd){
+                case "Enter":
+                    info.put(uid, nickName);
+                    chatLog.add(uid+"님이 들어왔습니다.");
+                    break;
+
+                case "Leave":
+                    chatLog.add(uid+"님이 나갔습니다.");
+                    break;
+                case "Change":
+                    info.put(uid, input[2]);
+                    break;
+
+            }
+        }
+
+        int size = chatLog.size();
+        answer = new String[size];
+        for(int i = 0; i < size; i++) {
+            String str = chatLog.get(i);
+            int endIdx = str.indexOf("님");
+            String userId = str.substring(0, endIdx);
+
+            answer[i] = str.replace(userId, info.get(userId));
+        }
+
+        return answer;
+
+    }
+}
